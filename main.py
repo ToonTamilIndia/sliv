779e687dafc5b1bfc791d764218b891224395e0a
ToonTamilIndia <121746774+ToonTamilIndia@users.noreply.github.com>
2026-04-26 09:53:48 +0530

    Update UPSTREAM_SOURCES

--- main.py
+++ main.py
@@ -31,15 +31,11 @@ logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s %(me
 
 UPSTREAM_SOURCES = [
     {
-        "template": "https://sliv.tulnit.fun/stream.php?id={id}&e=.m3u8",
-        "referer": "https://tulnit.com/",
-    },
-    {
         "template": "https://playyonogames.in/sliv/stream.php?id={id}&e=.m3u8",
         "referer": "https://playyonogames.in/",
     },
     {
-        "template": "https://mhdtvhub.com/sliv/stream.php?id={id}&e=.m3u8",
+        "template": "https://alymize.com/sliv/stream.php?id={id}&e=.m3u8",
         "referer": "https://mhdtvhub.com/",
     },
 ]
